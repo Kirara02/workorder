08c01c627473efc35e3240bbf6ae87b9ad62cf5a
Kirara02 <fathul.sirnaraja92@gmail.com>
2023-07-04 14:19:57 +0700

    change app title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }} | ProSmart</title>
+    <title>{{ $title }} | Eye System</title>
 
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('/') }}assets/img/favicon/favicon.ico" />
