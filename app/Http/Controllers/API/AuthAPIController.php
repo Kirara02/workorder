b23861b5e7a4bec8e99f6b090518761b46d0a2ab
Kirara02 <fathul.sirnaraja92@gmail.com>
2023-07-12 22:15:48 +0700

    fix user data login

--- app/Http/Controllers/API/AuthAPIController.php
+++ app/Http/Controllers/API/AuthAPIController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AuthResource;
+use App\Models\Company;
+use App\Models\Department;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,10 +19,11 @@ class AuthAPIController extends Controller
 
            if(Auth::guard('employee')->attempt(['nrp' => $request->input('nrp'),'password' => $request->input('password')])){
             $user = Auth::guard('employee')->user();
-
+            $user['company'] = Company::findOrFail($user->company_id);
+            $user['departemen'] = Department::findOrFail($user->department_id);
             $token = $user->createToken('MyApp',['employee'])->plainTextToken;
 
-            return response()->json(['message' => 'User logged in!', 'data' => $user, 'token' => $token]);
+            return response()->json(['message' => 'User logged in!', 'data' => new AuthResource($user), 'token' => $token]);
            }else{
                return response()->json(['error'=>'Invalid nrp or password.'],400);
            }
